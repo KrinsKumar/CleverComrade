8439019d3016020c702a31e7906771714df8e34d
Yutong Zhu <Yutong2022@gmail.com>
2023-11-04 16:00:57 -0400

    💻added testing api call and a nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 
 import SearchBar from "./components/SearchBar";
 import QuestionBox from "./components/AnswerBox";
-
+import NavBar from "./components/NavBar"
+import { useEffect } from "react";
 const instruction = "To receive a personalized answer from us, please start by uploading your document using the 'Upload' button provided. Once your file is successfully uploaded, a field will appear where you can type in your answer or relevant thoughts related to the document's content. Be as specific as possible to ensure the accuracy of the customized answer we generate for you. After submitting your information, hit the send button. Our system will then process your input and create a response tailored to the information you've shared. Please wait a moment for your customized answer to appear on the screen."
 
 function App() {
+
+
     return (
+      <>
+        <NavBar/>
       <div className="px-10 mt-3 text-sm">
-        {/* <p className="my-3">What can I help with?</p> */}
         <SearchBar/>
         <QuestionBox instruction={instruction} type={true}/>
         <QuestionBox instruction={instruction} type={false}/>
       </div>
+      </>
     )
 
 }
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 import SendSVG from '../svg/send-icon.svg'
 const SearchBar=()=>{
+    const onSubmitHandler=(prompt)=>{
+        axios.post(`https://witty-smock-ant.cyclic.app/query/`,{
+            params:prompt
+        })
+        .then((res)=>console.log(res))
+        .catch((err)=> console.log(err))
+      }
+
+    const [prompt, setPrompt] = useState('')
     return (
         <div className="relative">
             <input 
             type="text" 
             placeholder="Type your question..."
-            className="border border-black rounded-3xl w-full font-lg h-12 px-4 focus:outline-black "/>
-            <div style={{transform: 'translateY(-50%)'}} className="w-8 rounded-3xl absolute top-1/2 right-3 bg-blue-200 p-2 cursor-pointer">
+            className="border border-black rounded-3xl w-full font-lg h-12 px-4 focus:outline-black "
+            value={prompt}
+            onChange={(e)=>setPrompt(e.target.value)}/>
+            <div style={{transform: 'translateY(-50%)'}} className="w-8 rounded-3xl absolute top-1/2 right-3 bg-blue-200 p-2 cursor-pointer"
+            onClick={()=>{onSubmitHandler(prompt)}}>
                 <img className="" src={SendSVG} alt="Send Logo"/>
             </div>
         </div>
